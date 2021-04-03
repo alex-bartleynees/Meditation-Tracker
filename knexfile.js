2fdca3ebbce1506ee46d454158b6ef24fa116e56
alex-bartleynees <73970031+alex-bartleynees@users.noreply.github.com>
2021-04-03 15:21:28 +1300

    Set up database for heroku

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,19 @@ module.exports = {
       filename: ':memory:'
     },
     useNullAsDefault: true
+  },
+
+
+production: {
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
+}
+
 }
